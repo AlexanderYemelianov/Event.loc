b4169bb899726b2b0319ad0c87041dc5f1af8f09
AlexanderYemelianov <alexander.yemelianov@gmail.com>
2016-11-28 16:52:55 +0200

    synch admin home page

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -5,10 +5,8 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    <img src="#" alt="picture">
+                <div class="panel-body" align="center">
+					<img src="../picUploadTestDir/1.jpg" alt="picture" height="500px">
                 </div>
             </div>
         </div>
