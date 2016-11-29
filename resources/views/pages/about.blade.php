6955e17b6571d84c7a1c1250819fefda651ec320
AlexanderYemelianov <alexander.yemelianov@gmail.com>
2016-11-29 19:19:49 +0200

    about page view modification

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -22,5 +22,62 @@
         Ведь события не случаются , а создаются в Event Lab!
     </p>
 
+    <br>
+
+    <div class="row" align="center" id="aboutPage">
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="aboutPageText">
+                <h3>На связи 24/7</h3>
+                <br>
+                <p>Всегда мобильны</p>
+            </div>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/1.png">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="aboutPageText">
+                <h3>Не зависим от курса</h3>
+                <p>Всегда люльные и адаптируемся под любой бюджет</p>
+            </div>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/2.png">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="aboutPageText">
+                <h3>Экономим Ваше время и нервы</h3>
+                <p>Вам тоже нужен отдых-наслаждайтесь результатом!</p>
+            </div>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/3.png">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="aboutPageText">
+                <h3>Нестандартные решения</h3>
+                <p>Качество как правило, успех как закономерность</p>
+            </div>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/4.png">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="aboutPageText">
+                <h3>Энергии больше чем у дюрасель</h3>
+            </div>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/5.png">
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+            <div class="aboutPageText">
+                <h3>Мероприятие  «под ключ»</h3>
+            </div>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/6.png">
+        </div>
+
+        <div class="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-4 col-sm-offset-4 col-xs-6 col-xs-offset-3 ">
+            <h3>Always Happy!</h3>
+            <img class="img-responsive" src="../picUploadTestDir/aboutPage/7.png">
+        </div>
+
+    </div>
+
 @stop
 
